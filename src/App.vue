a0aae1fa85d29dfdf7efaaba17cdff88470d318a
M <m.trojanowicz98@gmail.com>
2020-04-15 18:45:40 +0200

    add-bootstrap

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@
 
 <script>
 import axios from 'axios'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 export default {
   data(){
