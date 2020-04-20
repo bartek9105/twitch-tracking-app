b8713d039d1bd3628b323cc12fe7ffa401fcc3b2
bartek9105 <bartek9105@gmail.com>
2020-04-20 12:20:54 +0200

    add compontents for button and logo

---
+++ src/components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+    <button class="btn btn-info mt-5 rounded-pill px-5 shadow">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'Button'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2 class="mb-5">Twitch<span class="text-info">Tracker</span></h2>
+</template>
+
+<script>
+export default {
+    name: 'Logo'
+}
+</script>
+
+<style>
+
+</style>
