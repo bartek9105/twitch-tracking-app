3dae5a6e017731e08c3a296995e40c5887b86e20
Piotr Rachwalik <piotr.rachwalik@gmail.com>
2020-04-28 16:48:30 +0200

    added logic for saving avatar for particular user

--- src/components/ProfileSettings.vue
+++ src/components/ProfileSettings.vue
@@ -3,21 +3,21 @@
     <h3 id="avatars-header" class="text-white text-center">Choose your avatar</h3>
     <div id="predefined-avatars" class="pt-4 pb-4">
       <div class="row justify-content-center">
-          <div class="col-xl-3 col-md-5 ml-md-3 ml-lg-5 ml-1">
-        <img
-          v-for="(url, index) in urls"
-          :key="url.id"
-          :src="url"
-          :id="url"
-          class="img m-2"
-          @click="toggle(index)"
-          :class="{'active': index == activeIndex}"
-        />
+        <div class="col-xl-3 col-md-5 ml-md-3 ml-lg-5 ml-1">
+          <img
+            v-for="(url, index) in urls"
+            :key="url.id"
+            :src="url"
+            :id="url"
+            class="img m-2"
+            @click="toggle(index, url)"
+            :class="{'active': index == activeIndex}"
+          />
         </div>
       </div>
     </div>
     <div class="row pb-5 justify-content-center">
-      <button class="btn btn-info rounded-pill mb-5 px-5 shadow">Save</button>
+      <button class="btn btn-info rounded-pill mb-5 px-5 shadow" @click="save">Save</button>
     </div>
   </div>
 </template>
@@ -29,7 +29,8 @@ export default {
   data() {
     return {
       urls: [],
-      activeIndex: null
+      activeIndex: null,
+      url: ""
     };
   },
   methods: {
@@ -40,7 +41,6 @@ export default {
       storageRef
         .listAll()
         .then(function(res) {
-          res.prefixes.forEach(function(folderRef) {});
           res.items.forEach(function(itemRef) {
             itemRef.getDownloadURL().then(function(url) {
               urls.push(url);
@@ -52,12 +52,38 @@ export default {
         });
       return (this.urls = urls);
     },
-    toggle: function(index) {
+    toggle: function(index, url) {
       this.activeIndex = index;
+      this.url = url;
+    },
+    save() {
+      if (this.url != "") {
+        const url = this.url;
+        var user = firebase.auth().currentUser;
+        const uid = user.uid;
+        firebase
+          .database()
+          .ref(`user_avatars/${uid}`)
+          .set({
+            avatarId: url
+          }).then(function() {
+              alert("Success")
+              //TODO add toasted messages
+            //   this.$toasted.global.success({
+            //         message: 'Avatar saved!'
+            //     })
+          }).catch(error => {
+              console.log(error)
+              alert("Failed")
+            //   this.$toasted.global.error({
+            //         message: 'Error occured!'
+            //     })
+          });
+      }
     }
   },
   mounted() {
-    this.listAllAvatars();
+    this.listAllAvatars()
   },
   computed: {}
 };
@@ -65,8 +91,8 @@ export default {
 
 <style>
 img {
-    width: 52px;
-    height: 77px;
+  width: 52px;
+  height: 77px;
 }
 img:hover {
   background-color: #38b2ac;
