e3683df46f597160c4a972ac348bcc102c75f67a
M <m.trojanowicz98@gmail.com>
2020-04-29 09:50:59 +0200

    clip-view

--- src/components/ClipInfo.vue
+++ src/components/ClipInfo.vue
@@ -1,16 +1,30 @@
 <template>
     <div id="clip-info" class="mt-5">
-        <div class="container-fluid mb-5">
+        <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-sm-10 offset-sm-1 col-12 m-auto">
                     <img :src="clip.thumbnails.medium" alt="clip-thumbnail" class="h-100 w-100">
                 </div>
-                <div class="col-lg-11 offset-lg-1 mt-5">
+                <div class="col-lg-11 offset-lg-1 col-md-11 offset-md-1 col-sm-11 offset-sm-1 col-11 offset-xs-1 mt-5 mb-5">
                     <h2 class="text-white">{{ clip.title }}</h2>
-                    <span id="clip-created" class="text-muted">
-                        <i class="far fa-calendar pr-1"></i>
-                        {{ editedClips.clip_created_at }}
-                    </span>
+                    <div class="mt-3">
+                        <span id="clip-created" class="text-muted">
+                            <i class="far fa-calendar pr-1"></i>
+                            {{ editedClips.clip_created_at }}
+                        </span>
+                    </div>
+                    <div class="mt-3">
+                        <span class="text-muted">
+                            <i class="fas fa-user text-muted pr-1"></i>
+                            {{clip.curator.name}}
+                        </span>
+                    </div>
+                    <div class="mt-3 mb-5">
+                        <span class="text-muted">
+                            <i class="fas fa-chart-area pr-1"></i>
+                            {{ clip.views }}
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
@@ -62,6 +76,5 @@ export default {
 <style>
     #clip-info{
         background-color: #2d3748;
-        min-height: 1000px;
     }
 </style>
